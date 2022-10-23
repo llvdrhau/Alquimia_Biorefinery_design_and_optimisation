67dabbc5f153dff28def47361099dded397a86ea
llvdrhau <lucasvdhauwaert@gmail.com>
2022-10-23 16:59:13 +0200

    find carbon of molecules with no formula

--- sbml_screening.py
+++ sbml_screening.py
@@ -38,6 +38,7 @@ def carbonBalance(model, case = 'secretion' ,tol = 1e-4):
                     else: continue
 
             carbonNrAll.append(nrOfCarbons)
+            # TODO should also be multiplied by the stoichiometric coef.
             gramsC = nrOfCarbons * 12 * reactionDF.flux[i]
             gramsCAll.append(gramsC)
 
@@ -89,6 +90,37 @@ def carbonBalanceReactions(model, metaboliteReactDict ,tol = 1e-4):
 
     return  dataFrameReturn
 
+def countCarbonInReactants(model,rctID):
+    rct = model.reactions.get_by_id(rctID)
+    flux = rct.flux
+    listMets = rct.reactants
+    productName = rct.products[0].name
+    carbonNrAll = []
+    for met in listMets:
+        metFormula = met.formula
+        splitFormula = re.split('(\d+)', metFormula)
+        nrOfCarbons = 0  # just in case something wierd happens
+        if 'C' not in metFormula:  # if there is no carbon in the formula
+            nrOfCarbons = 0
+        else:
+            for j, element in enumerate(splitFormula):
+                if 'C' in element and len(element) == 1:
+                    nrOfCarbons = int(splitFormula[j + 1])
+                elif 'C' in element and len(element) > 1:
+                    posCarbon = element.index('C')
+                    if element[posCarbon + 1].isupper():  # for case like CH4 there is no 1 next to the C
+                        nrOfCarbons = 1  # only one carbon
+                    else:
+                        continue  # for cases like Co (cobalt) just skip
+                else:
+                    continue
+
+        carbonNrAll.append(nrOfCarbons)
+    fluxCgrams = sum(carbonNrAll) * flux
+    totaalCarbonAtoms = sum(carbonNrAll)
+    #gramsC = nrOfCarbons * 12 * flux * stoiCoef
+    #gramsCAll.append(gramsC)
+    return productName, totaalCarbonAtoms, fluxCgrams
 #read model
 loc = os.getcwd()
 loc_acidi = loc + r'\SBML models\PAC_4875_model.xml'
@@ -98,10 +130,10 @@ model = cobra.io.read_sbml_model(loc_acidi)
 # # df = model.summary()
 # # print(df)
 #
-# uptakeDF = carbonBalance(model, case = 'uptake')
-# secretionDF = carbonBalance(model)
-# print(secretionDF)
-# #print(uptakeDF)
+uptakeDF = carbonBalance(model, case = 'uptake')
+secretionDF = carbonBalance(model)
+print(secretionDF)
+print(uptakeDF)
 #
 # CgramsOut = sum(secretionDF['flux (gram-C/g-DW/h)'])
 # CgramsIn = sum(uptakeDF['flux (gram-C/g-DW/h)'])    # should be 10*6*12 = 720  # 10 mols of glu, 6 C atoms, 12 gC/mol
@@ -132,3 +164,15 @@ rctWithMetId = {'S_cpd11461_c0': 'biomass_c0', # 'DNA', '
 
 df = carbonBalanceReactions(model= model, metaboliteReactDict= rctWithMetId)
 print(df)
+
+# for metabolites like protein cell wall count all carbons used to make the cell wall
+smallPoolMet = countCarbonInReactants(model, rctID= 'rxnnew72_c0')
+
+#newRow ={'Metabolite':smallPoolMet[0],'# of C':smallPoolMet[1], 'flux (gram-C/g-DW/h)':smallPoolMet[2] }
+#df2add = pd.DataFrame(newRow)
+#dfnew = pd.concat([df,df2add],ignore_index=True)
+#df.append(newRow, ignore_index=True)
+
+sm = model.metabolites.get_by_id('S_cpdnew27_c0').summary()
+print('')
+#smallPoolMet = countCarbonInReactants(model, rctID= 'rxnnew72_c0')
