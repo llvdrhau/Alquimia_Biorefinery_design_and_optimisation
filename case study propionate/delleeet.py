722352b7ee9ec94b18a189ad71707c354521c5fc
llvdrhau <lucasvdhauwaert@gmail.com>
2022-10-27 17:35:02 +0200

    compare massbalance to other models

---
+++ case study propionate/delleeet.py
@@ -0,0 +1,16 @@
+
+from f_find_carbons import carbonBalanceInOut
+import cobra
+import os
+cdw = os.getcwd()
+print(cdw)
+
+
+loc = os.getcwd()
+microViviane = loc + r'\SBML models\p-thermo.xml'  #iNF517.xml' #
+validat = cobra.io.sbml.validate_sbml_model(microViviane)
+print(validat[0])
+print('')
+print(validat[1]['SBML_FATAL']) #
+#model = cobra.io.read_sbml_model(microViviane)
+carbonBalanceInOut(modelLocation=microViviane, metIDsMissingCarbon=[], tol=1e-4)
